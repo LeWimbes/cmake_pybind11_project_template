73b99640329eb21059d1f31d0a73fccf2ed3d6a6
LeWimbes <leon.wimbes@web.de>
2022-09-27 16:47:07 +0300

    Fix string int concatenation

--- python/examples/main.py
+++ python/examples/main.py
@@ -2,6 +2,6 @@ import my_project as my
 
 print('Storing 7...')
 my.set_int(7)
-print('Stored Integer is: ' + my.get_int())
+print('Stored Integer is: ' + str(my.get_int()))
 
-print('1 + 2 = ' + my.add(1, 2))
+print('1 + 2 = ' + str(my.add(1, 2)))
